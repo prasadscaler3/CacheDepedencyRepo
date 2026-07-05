6f0d7364f8f07ab14f3eeff67c623a6700e47cca
prasadscaler3 <prasad.scaler3@gmail.com>
2026-07-05 22:40:47 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Maven cache demo");
+        System.out.println("Maven cache demo2");
     }
 }
 
